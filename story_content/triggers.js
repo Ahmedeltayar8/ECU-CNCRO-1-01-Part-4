5ce9539b5d8070474b12e11e907bb1d24e89852b
ahmedeltayar <ahmed.eltayar88@gmail.com>
2024-07-02 14:49:45 +0300

    first commit

---
+++ story_content/triggers.js
@@ -0,0 +1,81 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5ltnpBibAbC":
+        Script1();
+        break;
+      case "5fnIE7Cudkn":
+        Script2();
+        break;
+      case "6WYAJfdDxHd":
+        Script3();
+        break;
+      case "5laV8WDrLO9":
+        Script4();
+        break;
+      case "6HCP90xaGCz":
+        Script5();
+        break;
+      case "6CGDqSSBC3L":
+        Script6();
+        break;
+      case "5gFabm1pgUZ":
+        Script7();
+        break;
+      case "6fnJhjf7qmY":
+        Script8();
+        break;
+      case "6ATSIwK6CAt":
+        Script9();
+        break;
+      case "5qenOfYTS5M":
+        Script10();
+        break;
+      case "6naZJDqhsDq":
+        Script11();
+        break;
+      case "6bfV4KzjwtC":
+        Script12();
+        break;
+      case "5zwZTbkCuvc":
+        Script13();
+        break;
+      case "6LPl4bF6Gbo":
+        Script14();
+        break;
+      case "5nVYSFWHvvs":
+        Script15();
+        break;
+      case "5tjA7PrlJhO":
+        Script16();
+        break;
+      case "5r2O16kuwdD":
+        Script17();
+        break;
+      case "6oGRGX0kW0Q":
+        Script18();
+        break;
+      case "5bvFiVfAaGU":
+        Script19();
+        break;
+      case "6XJlYMAfRbO":
+        Script20();
+        break;
+      case "6NiVngGlBm4":
+        Script21();
+        break;
+      case "5nD6ul6gxCQ":
+        Script22();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
---
+++ story_content/user.js
@@ -0,0 +1,118 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  analytics();
+}
+
+window.Script2 = function()
+{
+  analytics();
+}
+
+window.Script3 = function()
+{
+  analytics();
+}
+
+window.Script4 = function()
+{
+  analytics();
+}
+
+window.Script5 = function()
+{
+  analytics();
+}
+
+window.Script6 = function()
+{
+  analytics();
+}
+
+window.Script7 = function()
+{
+  analytics();
+}
+
+window.Script8 = function()
+{
+  analytics();
+}
+
+window.Script9 = function()
+{
+  analytics();
+}
+
+window.Script10 = function()
+{
+  analytics();
+}
+
+window.Script11 = function()
+{
+  analytics();
+}
+
+window.Script12 = function()
+{
+  analytics();
+}
+
+window.Script13 = function()
+{
+  analytics();
+}
+
+window.Script14 = function()
+{
+  analytics();
+}
+
+window.Script15 = function()
+{
+  analytics();
+}
+
+window.Script16 = function()
+{
+  analytics();
+}
+
+window.Script17 = function()
+{
+  analytics();
+}
+
+window.Script18 = function()
+{
+  analytics();
+}
+
+window.Script19 = function()
+{
+  analytics();
+}
+
+window.Script20 = function()
+{
+  analytics();
+}
+
+window.Script21 = function()
+{
+  analytics();
+}
+
+window.Script22 = function()
+{
+  analytics();
+}
+
+};
